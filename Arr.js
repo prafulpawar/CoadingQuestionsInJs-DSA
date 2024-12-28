a2886a36896afcec69ebd3426631f6e393e01c82
prafulpawar <prafuldukhi@gmail.com>
2024-12-28 18:37:14 +0530

    Added A Questions

--- Arr.js
+++
@@ -1,6 +0,0 @@
-
-
-
-var minimumOperations = function(nums) {
-    
-};
---
+++ String/2315.js
@@ -0,0 +1,3 @@
+let prompt = require('prompt-sync')();
+let s = prompt("Enter a sentence to reverse the words ");
+console.log(s)
