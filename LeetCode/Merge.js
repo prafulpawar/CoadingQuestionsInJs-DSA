d058a4ae7339d3f3970576036b23a57478a25724
prafulpawar <prafuldukhi@gmail.com>
2024-12-28 11:48:09 +0530

    Added

--- LeetCode/Merge.js
+++ LeetCode/Merge.js
@@ -1,50 +1,36 @@
-// var merge = function(nums1, m, nums2, n) {
-//     let max = Math.max(m,n);
-//     let newArr=[];
-//     let j =0;
-//     let k =0;
-//     for(let i=0;i<max*2;i++){
-//        if(i<max){
+var merge = function (nums1, m, nums2, n) {
+    let i = m - 1; // Pointer for nums1 (valid elements)
+    let j = n - 1; // Pointer for nums2
+    let k = m + n - 1; // Pointer for the merged array (end of nums1)
 
-//           if(nums1[k] <= nums2[j] ){
-//              newArr[i] = nums1[i];
-//              k++;
-//           }
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i]; // Place the larger element at the end
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
 
-//           else{
-//              newArr[i] = nums2[j]
-//              j++
-//           }
-//        }
-
-//        else{
-//             if(k<m){
-//                  newArr[i] = nums1[k];
-//                  k++;
-//             }
-    
-//             else{
-//                  newArr[i] = nums2[j]
-//                  j++
-//             }
-//        }
+    // If elements are left in nums2, copy them
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+  
+};
 
 
-//     }  
-
-//     return newArr    
-// };
-
-
-
-
-let nums1 = [1, 0]; 
-let m = 2; 
-let nums2 = [2]; 
-let n = 1;
+let nums1 = [4, 0, 0, 0, 0, 0,];
+let m = 6;
+let nums2 = [1, 2, 3,5,6];
+let n = 3;
 
 
 let result = merge(nums1, m, nums2, n);
 
 
- console.log(result); 
+console.log(result); 
