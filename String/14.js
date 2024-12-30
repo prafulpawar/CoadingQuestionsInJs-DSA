7e17d0234e50fda0323279478bfed051e45a8d6c
prafulpawar <prafuldukhi@gmail.com>
2024-12-31 00:33:25 +0530

    Commit New Question Addded

---
+++ String/14.js
@@ -0,0 +1,8 @@
+var longestCommonPrefix = function(strs) {
+     let k=strs.length-1;
+  for(let i=0;i<strs.length;i++){
+   
+  }
+
+};
+longestCommonPrefix(["flower","flow","flight"]);
--- String/2315.js
+++ String/2315.js
@@ -1,3 +1,27 @@
-let prompt = require('prompt-sync')();
-let s = prompt("Enter a sentence to reverse the words ");
-console.log(s)
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countAsterisks = function(s) {
+    let nons=0
+   
+     for(let start = 0; start < s.length; start++ ) {
+           
+           if(s[start] ==='|'){
+               
+               let end=start+1
+              
+               while(end < s.length && s.charAt(end) !=='|'){
+                   end++;
+               }
+               start =end
+           }
+           else{
+               if(s[start]=='*'){
+                   nons++     
+               }
+           }
+          
+       }
+     return nons
+};
