b0dbc2b18bb4bb2df0bc4e2dd3ae36b340adbc62
prafulpawar <prafuldukhi@gmail.com>
2024-12-31 00:34:55 +0530

    Added Something

--- Arrys/2293.js
+++ Arrys/2293.js
@@ -7,34 +7,8 @@ for (let i = 0; i < size; i++) {
 }
 
 var minMaxGame = function (nums) {
-    if(nums.length ===2){
-       return nums[1]
-    }
-    let newArr=[]
-    let k = 0
-    let j =0
-   let i =0;
-    while (i!=1) {    
-          if(nums[k]<nums[j]){
-            newArr[i] = nums[k];
-            
-          }
-          else{
-            newArr[i] = nums[j];
-           
-          }
-          k+=2 
-          j+=2
-          i++;
-         if(i==nums.length/2-1){
-             nums = newArr
-             k=0;
-             j=0;
-             i=0;
-             console.log(nums)
-         }
-    }     
-    return nums[0];
+    let newArr=[];
+    newArr.push(nums.length/2);
 };
 
 minMaxGame(nums);
